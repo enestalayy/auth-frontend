e6a00e343bc40a5724777717c79c50c80dd5bb00
Enes Talay <enestalayy@gmail.com>
2024-12-03 22:47:29 +0300

    Update profile.get.ts

--- server/api/users/profile.get.ts
+++ server/api/users/profile.get.ts
@@ -6,6 +6,7 @@ export default defineEventHandler(async (event) => {
   return await $fetch(`${apiUrl}/users/profile`, {
     headers: {
       "x-api-key": apiKey,
+      Authorization: `Bearer ${accessToken}`,
     },
   });
 });
